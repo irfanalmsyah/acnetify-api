8093905821d33b5671825a2e1996ddf26e79403f
aamilham <aamilham13@gmail.com>
2024-06-03 17:03:00 +0700

    change format to env

--- src/services/bucket.service.ts
+++ src/services/bucket.service.ts
@@ -1,8 +1,9 @@
+import { env } from "@config/env";
 import { Storage } from "@google-cloud/storage"
 
 const storage = new Storage({
-  projectId: 'capstone-acnetify',
-  keyFilename: 'keyfile.json'
+  projectId: env.GOOGLE_CLOUD_PLATFORM_PROJECT_ID,
+  keyFilename: env.GCP_KEY_FILE_PATH
 });
 
 // Memilih bucket yang akan digunakan
