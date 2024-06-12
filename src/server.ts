15344010fa558bc9a9a12652b922b25cac523121
aamilham <aamilham13@gmail.com>
2024-06-12 17:16:48 +0700

    add functions to addModel

--- src/server.ts
+++ src/server.ts
@@ -3,32 +3,28 @@ import express from "express"
 import cors from "cors"
 import httpContext from "express-http-context"
 import baseRoutes from "./routes"
-import { loadModel } from "@services/loadmodel.service"
+import { addModelToApp } from "@services/image.service"
 
-const app = express();
+const app = express()
 
 
-app.use(morgan("short"));
+app.use(morgan("short"))
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(httpContext.middleware);
+app.use(httpContext.middleware)
 
-(async () => {
-    console.log("--async passed--")
-    const model = await loadModel()
-    console.log("--loadModel passed--")
-    app.set("model", model)
-    console.log("Model loaded successfully")
-})();
+;(async () => {
+    await addModelToApp(app)
+})()
 
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
         credentials: true,
     })
-);
+)
 
-app.use(baseRoutes);
+app.use(baseRoutes)
 
 export default app
