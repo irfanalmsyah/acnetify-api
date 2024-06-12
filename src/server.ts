20549e086fcad591bb8386dd98f4deb9118556e0
aamilham <aamilham13@gmail.com>
2024-06-12 16:56:34 +0700

    add loadmodel into server when it started

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,32 @@ import express from "express"
 import cors from "cors"
 import httpContext from "express-http-context"
 import baseRoutes from "./routes"
+import { loadModel } from "@services/loadmodel.service"
 
-const app = express()
+const app = express();
 
-app.use(morgan("short"))
 
-app.use(express.json())
+app.use(morgan("short"));
 
-app.use(httpContext.middleware)
+app.use(express.json());
+
+app.use(httpContext.middleware);
+
+(async () => {
+    console.log("--async passed--")
+    const model = await loadModel()
+    console.log("--loadModel passed--")
+    app.set("model", model)
+    console.log("Model loaded successfully")
+})();
 
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
         credentials: true,
     })
-)
+);
 
-app.use(baseRoutes)
+app.use(baseRoutes);
 
 export default app
